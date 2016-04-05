6fb7336e5df71ecefa661aa47bb5ca8aa4cc739f
theadedmonkey <theadedmonkey@gmail.com>
2016-04-05 17:35:19 +0200

    added boards collection

---
+++ imports/api/boards/boards.js
@@ -0,0 +1,40 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+class BoardsCollection extends Mongo.Collection {
+  insert(doc, callback) {
+    const board = doc;
+    board.createdAt = board.createdAt || new Date();
+    const result = super.insert(board, callback);
+    return result;
+  }
+}
+
+const Boards = new BoardsCollection('Boards');
+
+Boards.schema = new SimpleSchema({
+  name: {
+    type: String,
+    min: 3,
+    max: 10,
+    unique: true,
+  },
+
+  description: {
+    type: String,
+    optional: true,
+  },
+
+  createdAt: {
+    type: Date,
+  },
+
+  userId: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+  },
+});
+
+Boards.attachSchema(Boards.schema);
+
+export { Boards };
--- imports/api/pins/pins.js
+++ imports/api/pins/pins.js
@@ -1,14 +1,22 @@
 import { Mongo } from 'meteor/mongo';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
-const Pins = new Mongo.Collection('Pins');
+class PinsCollection extends Mongo.Collection {
+  insert(doc, callback) {
+    const pin = doc;
+    pin.createdAt = pin.createdAt || new Date();
+    const result = super.insert(pin, callback);
+    return result;
+  }
+}
+
+const Pins = new PinsCollection('Pins');
 /*
  * We attach the schema to the namespace of Pins directly, which allows us to
  * check objects against this schema directly whenever we want, such as in a
  * form or Method.
  */
 Pins.schema = new SimpleSchema({
-
   title: {
     type: String,
     min: 3,
@@ -23,19 +31,6 @@ Pins.schema = new SimpleSchema({
 
   createdAt: {
     type: Date,
-    optional: true,
-    /*
-     * FIXME:
-     * the guide not make any use of the autovalue simpleschema functionality,
-     * instead it creates a subclass of Mongo.Collection where overwrites the
-     * insert method.
-     * see: https://github.com/meteor/todos/blob/master/imports/api/todos/todos.js
-     */
-    autoValue: function() {
-      if (this.isInsert) {
-        return new Date;
-      }
-    },
   },
   /*
    * TODO:
@@ -46,7 +41,8 @@ Pins.schema = new SimpleSchema({
     type: String,
     regEx: SimpleSchema.RegEx.Id,
   },
-
 });
 
 Pins.attachSchema(Pins.schema);
+
+export { Pins };
