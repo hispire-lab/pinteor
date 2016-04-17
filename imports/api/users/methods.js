3e2637af2f50e73955bb8bff6d9d978ddae9298c
theadedmonkey <theadedmonkey@gmail.com>
2016-04-17 19:59:32 +0200

    added Users.methods.unfollow

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -60,4 +60,61 @@ const follow = new ValidatedMethod({
   },
 });
 
-export { follow };
+/*
+ * TODO:
+ * Attach method to a namespace, like Users.methods.unfollow
+ */
+const unfollow = new ValidatedMethod({
+  // The name of the method, sent over the wire. Same as the key provided
+  // when calling Meteor.methods
+  name: 'Users.methods.unfollow',
+  // Validation function for the arguments. Only keyword arguments are accepted,
+  // so the arguments are an object rather than an array. The SimpleSchema validator
+  // throws a ValidationError from the mdg:validation-error package if the args don't
+  // match the schema
+  //
+  // This Method encodes the form validation requirements.
+  // By defining them in the Method, we do client and server-side
+  // validation in one place.
+  validate: new SimpleSchema({
+    dstUserId: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Id,
+    },
+  }).validator(),
+  // This is the body of the method. Use ES2015 object destructuring to get
+  // the keyword arguments
+  run({ dstUserId }) {
+    // `this` is the same method invocation object you normally get inside
+    // Meteor.methods
+    if (!this.userId) {
+      throw new Meteor.Error(
+        'Users.methods.follow.not-logged-in',
+        'Must be logged in to follow an user.'
+      );
+    }
+
+    const dstUser = Users.findOne({ _id: dstUserId });
+    if (!dstUser) {
+      throw new Meteor.Error(
+        'Users.methods.follow.not-found',
+        'Cannot unfollow a non existing user.'
+      );
+    }
+    /*
+     * this mutator operation could be inside Users.update hook, i put it here
+     * beacuse i don't know how to subclass Meteor.users collection.
+     */
+    Users.update(
+      { _id: dstUserId },
+      { $pull: { followers: this.userId } }
+    );
+
+    return Users.update(
+      { _id: this.userId },
+      { $pull: { following: dstUserId } }
+    );
+  },
+});
+
+export { follow, unfollow };
--- imports/api/users/methods.tests.js
+++ imports/api/users/methods.tests.js
@@ -7,7 +7,7 @@ import { chai } from 'meteor/practicalmeteor:chai';
 import { Random } from 'meteor/random';
 import faker from 'faker';
 import { Users } from './users.js';
-import { follow } from './methods.js';
+import { follow, unfollow } from './methods.js';
 
 Factory.define('user', Users, {
   username: faker.name.firstName(),
@@ -16,7 +16,7 @@ Factory.define('user', Users, {
 
 if (Meteor.isServer) {
   describe('Users.methods', function () {
-    describe('Users.methods.insert', function () {
+    describe('Users.methods.follow', function () {
       beforeEach(function () {
         resetDatabase();
       });
@@ -34,7 +34,7 @@ if (Meteor.isServer) {
         follow._execute({ userId: srcUser._id }, { dstUserId: dstUser._id });
 
         chai.assert.equal(
-          Users.findOne({ following: dstUser._id })._id,
+          Users.findOne({ _id: srcUser._id, following: dstUser._id })._id,
           srcUser._id
         );
       });
@@ -45,8 +45,42 @@ if (Meteor.isServer) {
         follow._execute({ userId: srcUser._id }, { dstUserId: dstUser._id });
 
         chai.assert.equal(
-          Users.findOne({ followers: srcUser._id })._id,
+          Users.findOne({ _id: dstUser._id, followers: srcUser._id })._id,
           dstUser._id
+        );
+      });
+    });
+    describe('Users.methods.unfollow', function () {
+      beforeEach(function () {
+        resetDatabase();
+      });
+      it('should not unfollow an user if user not exists', function () {
+        const srcUser = Factory.create('user');
+
+        chai.assert.throws(() => {
+          unfollow._execute({ userId: srcUser._id }, { dstUserId: Random.id() });
+        }, Meteor.Error, /Cannot unfollow a non existing user./);
+      });
+      it('should unfollow another user', function () {
+        const srcUser = Factory.create('user', { username: 'username1' });
+        const dstUser = Factory.create('user', { username: 'username2' });
+        follow._execute({ userId: srcUser._id }, { dstUserId: dstUser._id });
+
+        unfollow._execute({ userId: srcUser._id }, { dstUserId: dstUser._id });
+
+        chai.assert.isUndefined(
+          Users.findOne({ id: srcUser._id, following: dstUser._id })
+        );
+      });
+      it('should unfollow another user an not be a follower.', function () {
+        const srcUser = Factory.create('user', { username: 'username1' });
+        const dstUser = Factory.create('user', { username: 'username2' });
+        follow._execute({ userId: srcUser._id }, { dstUserId: dstUser._id });
+
+        unfollow._execute({ userId: srcUser._id }, { dstUserId: dstUser._id });
+
+        chai.assert.isUndefined(
+          Users.findOne({ _id: dstUser._id, followers: srcUser._id })
         );
       });
     });
